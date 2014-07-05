4e6bfbd5951c991f45d3bc247228f63156769f60
hidetomo <hidetomo@www4322gi.sakura.ne.jp>
2014-07-05 16:59:42 +0900

    add polynomial

--- least-squares-method/lsm-polynomial.go
+++ least-squares-method/lsm-polynomial.go
@@ -7,7 +7,7 @@ import (
   "image/color"
   "math/rand"
   "math"
-  //"fmt"
+  "fmt"
 )
 
 func normalRand(mu, sigma float64) float64 {
@@ -22,18 +22,59 @@ func linspace(start, end float64, n int, x plotter.XYs) {
   }
 }
 
-/*
-func gaussElimination(mat [][]float64, height, width int) []float64 {
-  // Pivoting
-  for x := 0; x < width; x++ {
-    pivot := x
 
-    for y := x; y < height; y++ {
-      
+func gaussElimination(mat [][]float64, numParams int) []float64 {
+  // Pivoting
+  for x := 0; x < numParams; x++ {
+    pivot := x
+    maxVal := 0.0
+    for y := x; y < numParams; y++ {
+      if math.Abs(mat[y][x]) > maxVal {
+        maxVal = math.Abs(mat[y][x])
+        pivot = y
+      }
+    }
+
+    // switch column when pivot != x
+    if pivot != x {
+      for k := 0; k < numParams+1; k++ {
+        tmp           := mat[x][k]
+        mat[x][k]     =  mat[pivot][k]
+        mat[pivot][k] =  tmp
+      }
     }
   }
+
+  // Make upper triangular matrix with basic deformation of matrix
+  for k := 0; k < numParams; k++ {
+    // Save diagonal element
+    diagElem := mat[k][k]
+    // Diagonal element must be 1.0
+    mat[k][k] = 1.0
+    // Basic deformation
+    for x := k+1; x < numParams+1; x++ { mat[k][x] /= diagElem  }
+    //
+    for y := k+1; y < numParams; y++ {
+      tmp := mat[y][k]
+      for x := k+1; x < numParams+1; x++ {
+        mat[y][x] -= tmp*mat[k][x]
+      }
+      mat[y][k] = 0.0
+    }
+  }
+
+  // Solve
+  result := make([]float64, numParams, numParams)
+  for y := numParams-1; y >= 0; y-- {
+    result[y] = mat[y][numParams]
+    for x := numParams-1; x > y; x-- {
+      result[y] -= mat[y][x] * result[x]
+    }
+  }
+
+  return result
 }
-*/
+
 
 func main() {
   //===================================================
@@ -58,52 +99,58 @@ func main() {
   //====================================================
   // LSM
 
-  // Prepare X axis of LSM result points
-  N := 1000
-  result := make(plotter.XYs, N)
-  linspace(-3, 3, N, result)
-
-  // Trigonometric Polynomial Basis Function for solving
-  numParams := 31
-  basisFunc := make([][]float64, n, n)
-  for y := 0; y < n; y++ {
-    basisFunc[y] = make([]float64, numParams, numParams)
-    for x := 0; x <= (numParams-1)/2; x++ {
-      if x == 0 {
-        basisFunc[y][x] = 1.0
-      } else {
-        basisFunc[y][2*x-1] = math.Sin(float64(x)/2.0 * answer[y].X)
-        basisFunc[y][2*x]   = math.Cos(float64(x)/2.0 * answer[y].X)
-      }
-    }
-  }
-
-  // Make Normal Equation
-  equation := make([][]float64, numParams, numParams)
+  // Make a matrix to solve
+  numParams := 5
+  mat := make([][]float64, numParams, numParams)
+  fmt.Printf("[Matrix for Gausss Elimination]\n")
   for y := 0; y < numParams; y++ {
-    equation[y] = make([]float64, numParams+1, numParams+1)
-    for x := 0; x < numParams; x++ {
-      summation := 0.0
-      for k := 0; k < n; k++ {
-        summation += basisFunc[k][x] * basisFunc[k][y]
+    mat[y] = make([]float64, numParams+1, numParams+1)
+    for x := 0; x < numParams+1; x++ {
+      mat[y][x] = 0.0
+      if x < numParams {
+        for k := 0; k < n; k++ {
+          mat[y][x] += math.Pow(answer[k].X, float64(y+x))
+        }
+      } else {
+        for k := 0; k < n; k++ {
+          mat[y][x] += math.Pow(answer[k].X, float64(y)) * answer[k].Y
+        }
       }
-      equation[y][x] = summation
+      fmt.Printf("%f\t", mat[y][x])
     }
+    fmt.Printf("\n")
   }
+  fmt.Printf("\n")
 
   // Solve y=phi*theta
-  //theta := gaussElimination(gaussMat)
+  params := gaussElimination(mat, numParams)
+  
+  // Output
+  fmt.Printf("[Optimized Params]\n")
+  for i := 0; i < numParams; i++ {
+    fmt.Printf("param %d : %f\n", i, params[i])
+  }
 
   //====================================================
   // Graph Setting
+
+  // Result graph
+  N := 1000
+  result := make(plotter.XYs, N)
+  linspace(-3, 3, N, result)
+  for i := 0; i < N; i++ {
+    result[i].Y = 0.0
+    for k := 0; k < numParams; k++ {
+      result[i].Y += params[k]*math.Pow(result[i].X, float64(k))
+    }
+  }
 
   // Create a new plot, set its title and axis labels
   p, err := plot.New()
   if err != nil {
     panic(err)
   }
-  p.Title.Text = "Plotinum Sample"
-  //p.Title.Text = "Least Square Method"
+  p.Title.Text = "Least Square Method"
   p.X.Label.Text = "X"
   p.Y.Label.Text = "Y"
   p.Add(plotter.NewGrid())
@@ -117,13 +164,21 @@ func main() {
   lpLine.Color = color.RGBA{G: 255, A: 255}
   lpPoints.Shape = plot.PyramidGlyph{}
   lpPoints.Color = color.RGBA{R: 255, A: 255}
-  
+ 
+  // Result line
+  lpResultLine, _, err := plotter.NewLinePoints(result)
+  if err != nil {
+    panic(err)
+  }
+  lpResultLine.Color = color.RGBA{B: 255, A: 255}
+ 
   // Add data and legend
-  p.Add(lpPoints)
-  p.Legend.Add("observed points", lpPoints)
+  p.Add(lpPoints, lpResultLine)
+  p.Legend.Add("Observed Points", lpPoints)
+  p.Legend.Add("Result", lpResultLine)
   
   // Save the plot to a PNG file.
-  if err := p.Save(4, 4, "points2.png"); err != nil {
+  if err := p.Save(4, 4, "lsm-polynomial.png"); err != nil {
     panic(err)
   }
 
